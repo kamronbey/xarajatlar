1011353d1f13ce45e2abf38b630afb07f1487907
Kamronbek <80781219+kamronbey@users.noreply.github.com>
2021-04-16 11:10:11 +0500

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var dollar = 9433.34;
+var yevro = 10354.03
+var totalCosts = 8317488;
+var totalMoney = Number (prompt (`Enter your total money:`))
+
+if (totalMoney >= totalCosts) {
+   console.log(`Sizning umumiy pulingiz: ${totalMoney} sum.Oq yo'l Alisher`);
+} else {
+   console.log(`Sizning umumiy pulingiz: ${totalMoney} sum.Alisher ozgina sabr qilish kerak bolar ekan`);
+}
