79600820b4ba2fe8058ccf762c277895eb3c9809
Kamronbek <80781219+kamronbey@users.noreply.github.com>
2021-04-16 11:51:39 +0500

    sass

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ var yevro = 10354.03
 var totalCosts = 8317488;
 var totalMoney = Number (prompt (`Enter your total money:`))
 
+
 if (totalMoney >= totalCosts) {
    console.log(`Sizning umumiy pulingiz: ${totalMoney} sum.Oq yo'l Alisher`);
 } else {
    console.log(`Sizning umumiy pulingiz: ${totalMoney} sum.Alisher ozgina sabr qilish kerak bolar ekan`);
 }
+
+confirm ("Salom qalaysiz?")
+
